066a1cdc2b34f92ce3d9832ac807e6c1f2e7ea6f
Benjamin Weller <benjamin.weller@mgm-sp.com>
2023-10-09 11:26:17 +0200

    Adds rotation

--- watermark.sh
+++ watermark.sh
@@ -12,13 +12,15 @@ TARGET_FILE=${FILE%.*}-watermark.${FILE##*.}
 WIDTH=$(bc <<< "$(identify $FILE | cut -d " " -f 3 | sed -E "s/([0-9]+)x([0-9]+)/\1/g") * 0.5")
 HEIGHT=$(bc <<< "$(identify $FILE | cut -d " " -f 3 | sed -E "s/([0-9]+)x([0-9]+)/\2/g") * 0.5")
 POINTSIZE=$(bc <<< "x = ($HEIGHT - $WIDTH) * 0.4; if (x < 0) -x else x")
-X1=$(bc <<< "60 + $RANDOM % 30")
-X2=$(bc <<< "60 + $RANDOM % 30")
-Y1=$(bc <<< "60 + $RANDOM % 30")
-Y2=$(bc <<< "60 + $RANDOM % 30")
+X1=$(bc <<< "20 + $RANDOM % 30")
+X2=$(bc <<< "20 + $RANDOM % 30")
+Y1=$(bc <<< "20 + $RANDOM % 30")
+Y2=$(bc <<< "20 + $RANDOM % 30")
+ROT=$(bc <<< "10 + $RANDOM % 20")
 
 convert -size ${WIDTH}x${HEIGHT} xc:none -fill "#00000050" \
       -gravity NorthWest -pointsize $POINTSIZE -draw "text $X1,$Y1 '$TEXT'" \
       -gravity SouthEast -pointsize $POINTSIZE -draw "text $X2,$Y2 '$TEXT'" \
-      miff:- |\
+      -distort SRT -$ROT miff:- |\
 composite -tile - $FILE $TARGET_FILE
+
